4e236387508535633acf4f9c90e569579470049d
Rishav Raj <97820668+Rishav705@users.noreply.github.com>
2023-10-16 05:38:57 +0000

    Coversion Of Datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Rishav"
+
+//console.log(typeof score)
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Rishav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Rishav" => true
+
+let stringNo = 33
+
+let stringNumber = String(stringNo)
+console.log(stringNo);
+console.log(typeof stringNo);
